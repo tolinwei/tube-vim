b35a108584c71e43b54b75f76ae951f98752c0b8
Wei Lin <tolinwei@gmail.com>
2015-07-09 14:55:02 -0700

    Formated installation scripts, put files into 'colors' and 'config' folders

--- install-linux.sh
+++ install-linux.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+
 echo -e "
      _         _                     _
     | |_ _   _| |__   ___     __   _(_)_ __ ___
@@ -8,20 +9,28 @@ echo -e "
 
 echo -e "## [START] Seting up shell-based development environment from within this repo...\n"
 
+
+
 echo -e "## [STAGE] Defining folder variables..."
 HOME_DIR=~
 PROJECT_DIR=${HOME_DIR}/.tube-vim/
+PROJECT_COLOR_DIR=${PROJECT_DIR}/colors
+PROJECT_CONFIG_DIR=${PROJECT_DIR}/config
+
 DOWNLOAD_DIR=${HOME_DIR}/Downloads/
 VIM_COLOR_DIR=${HOME_DIR}/.vim/colors/
 ZSH_THEME_DIR=${HOME_DIR}/.oh-my-zsh/themes/
 echo -e "## [STAGE] ...Done\n"
+
+
 
 echo -e "## [STAGE] Cloning repo to home directories..."
 rm -rf $PROJECT_DIR
 git clone https://github.com/tolinwei/tube-vim.git $PROJECT_DIR
 echo -e "## [STAGE] ...Done\n"
 
-echo -e "## [STAGE] Backing up existing Vim, screen, tmux settings..."
+
+echo -e "## [STAGE] Backing up existing Vim, screen, tmux settings and bashrc..."
 cd $HOME_DIR
 date_time=`date +"%y-%m-%d-%H:%M"`
 if [ -e .vimrc ] ; then
@@ -40,33 +49,46 @@ if [ -e .tmux.conf ] ; then
     echo -e "## [INFO] Backing up .tmux.conf to .tmux.conf.bak-${date_time}"
     mv .tmux.conf .tmux.conf.bak-${date_time}
 fi
+if [ -e .bashrc ] ; then
+    echo -e "## [INFO] Backing up .bashrc to .bashrc.bak-${date_time}"
+    mv .bashrc .bashrc.bak-${date_time}
+fi
 echo -e "## [STAGE] ...Done\n"
+
+
 
 echo -e "## [STAGE] Setting up directories..."
 cd $HOME_DIR
 mkdir -p $DOWNLOAD_DIR
 echo -e "## [STAGE] ...Done\n"
 
-echo -e "## [STAGE] Copying configuration file for bash and color scheme for Vim..."
-cd $PROJECT_DIR
-if [ -e .bashrc ] ; then
-    echo -e "## [INFO] Backing up existing .bashrc to .bashrc.bak-${date_time}"
-    mv .bashrc .bashrc.bak-${date_time}
-fi
+
+
+echo -e "## [STAGE] Copying configuration file for bash, tmux, screen and color scheme for Vim..."
+cd $PROJECT_CONFIG_DIR
 cat bashrc >> ${HOME_DIR}/.bashrc
+cp tmux.conf ${HOME_DIR}/.tmux.conf
+cp screenrc ${HOME_DIR}/.screenrc
+
 mkdir -p $VIM_COLOR_DIR
 cp gruvbox.vim $VIM_COLOR_DIR
 echo -e "## [STAGE] ...Done\n"
 
+
+
 echo -e "## [STAGE] Installing Vundle for manegement of Vim plugins..."
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo -e "## [STAGE] ...Done\n"
+
+
 
 echo -e "## [STAGE] Installing Vim plugins defined in vimrc..."
 cd $PROJECT_DIR
 cp vimrc-linux ${HOME_DIR}/.vimrc
 vim +PluginInstall +qall
 echo -e "## [STAGE] ...Done\n"
+
+
 
 #echo -e "## Compiling ycm_support_libs, then compile YouCompleteMe..."
 #mkdir -p $YCM_DIR
@@ -75,6 +97,8 @@ echo -e "## [STAGE] ...Done\n"
 #make ycm_support_libs
 #cd $HOME_DIR/.vim/bundle/YouCompleteMe
 #./install.sh
+
+
 
 echo -e "## [STAGE] Installing oh-my-zsh, and copying configuration file..."
 if [ -d ${HOME_DIR}/.oh-my-zsh ]; then
@@ -85,9 +109,11 @@ else
     echo -e "## [INFO] Installing new oh-my-zsh only"
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" && exit
 fi
-cd $PROJECT_DIR
+cd $PROJECT_CONFIG_DIR
 cp zshrc ${HOME_DIR}/.zshrc
 echo -e "## [STAGE] ...Done\n"
+
+
 
 echo -e "## [DONE] Finish installation, please enjoy!!"
 echo -e "
@@ -96,4 +122,5 @@ echo -e "
     | __| | | | '_ \\ / _ \\____\\ \\ / / | '_ \` _ \\
     | |_| |_| | |_) |  __/_____\\ V /| | | | | | |
      \\__|\\__,_|_.__/ \\___|      \\_/ |_|_| |_| |_|\n"
+
 
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo -e "
      _         _                     _
     | |_ _   _| |__   ___     __   _(_)_ __ ___
@@ -8,13 +9,20 @@ echo -e "
 
 echo -e "## [START] Seting up shell-based development environment from within this repo...\n"
 
+
+
 echo -e "## [STAGE] Defining directory variables..."
 HOME_DIR=~
-PROJECT_DIR=${HOME_DIR}/.tube-vim/
-DOWNLOAD_DIR=${HOME_DIR}/Downloads/
-VIM_COLOR_DIR=${HOME_DIR}/.vim/colors/
-ZSH_THEME_DIR=${HOME_DIR}/.oh-my-zsh/themes/
+PROJECT_DIR=${HOME_DIR}/.tube-vim
+PROJECT_COLOR_DIR=${PROJECT_DIR}/colors
+PROJECT_CONFIG_DIR=${PROJECT_DIR}/config
+
+DOWNLOAD_DIR=${HOME_DIR}/Downloads
+VIM_COLOR_DIR=${HOME_DIR}/.vim/colors
+ZSH_THEME_DIR=${HOME_DIR}/.oh-my-zsh/themes
 echo -e "## [STAGE] ...Done\n"
+
+
 
 echo -e "## [STAGE] Installing brew, git, wget for OS X..."
 which -s brew
@@ -30,16 +38,22 @@ echo -e "## [INFO] Installing git via Homebrew"
 which -s git || brew install git
 echo -e "## [STAGE] ...Done\n"
 
+
+
 echo -e "## [STAGE] Cloning repo to home directories..."
 rm -rf $PROJECT_DIR
 git clone https://github.com/tolinwei/tube-vim.git $PROJECT_DIR
 echo -e "## [STAGE] ...Done\n"
 
+
+
 echo -e "## [STAGE] Installing Command Line Tools for OS X (please click 'Install' on popup window)..."
 xcode-select --install
 echo -e "## [STAGE] ...Done\n"
 
-echo -e "## [STAGE] Backing up existing Vim, screen, tmux settings..."
+
+
+echo -e "## [STAGE] Backing up existing Vim, screen, tmux settings and bashrc..."
 cd $HOME_DIR
 date_time=`date +"%y-%m-%d-%H:%M"`
 if [ -e .vimrc ] ; then
@@ -58,12 +72,20 @@ if [ -e .tmux.conf ] ; then
     echo -e "## [INFO] Backing up .tmux.conf to .tmux.conf.bak-${date_time}"
     mv .tmux.conf .tmux.conf.bak-${date_time}
 fi
+if [ -e .bashrc ] ; then
+    echo -e "## [INFO] Backing up .bashrc to .bashrc.bak-${date_time}"
+    mv .bashrc .bashrc.bak-${date_time}
+fi
 echo -e "## [STAGE] ...Done\n"
+
+
 
 echo -e "## [STAGE] Setting up directories..."
 cd $HOME_DIR
 mkdir -p $DOWNLOAD_DIR
 echo -e "## [STAGE] ...Done\n"
+
+
 
 echo -e "## [STAGE] Installing Vim and tmux via Homebrew..."
 ## https://github.com/Homebrew/homebrew/blob/master/Library/Formula/vim.rb
@@ -76,6 +98,8 @@ brew install vim
 brew install tmux
 echo -e "## [STAGE] ...Done\n"
 
+
+
 echo -e "## [STAGE] Installing Exuberant Ctags to support tagbar via Homebrew..."
 CTAGS_DIR=`which ctags`
 if [ $CTAGS_DIR != '/usr/local/bin/ctags' ] ; then
@@ -85,24 +109,26 @@ if [ $CTAGS_DIR != '/usr/local/bin/ctags' ] ; then
     #cd ctags-5.8
     #./configure -prefix=/usr/local
     #make && sudo make install
-    echo -e "## [INFO] Installing new Ctags";
+    echo -e "## [INFO] Installing new Ctags via Homebrew";
     brew install ctags
 else
-    echo -e "## [INFO] Exuberant Ctags has already installed"
+    echo -e "## [INFO] Exuberant Ctags (Homebrew version) has already installed"
 fi
 echo -e "## [STAGE] ...Done\n"
 
-echo -e "## [STAGE] Copying configuration file for bash and color scheme for Vim..."
-cd $HOME_DIR
-if [ -e .bashrc ] ; then
-    echo -e "## [INFO] Backing up existing .bashrc to .bashrc.bak-${date_time}"
-    mv .bashrc .bashrc.bak-${date_time}
-fi
-cd $PROJECT_DIR
+
+
+echo -e "## [STAGE] Copying configuration file for bash, tmux, screen and color scheme for Vim..."
+cd $PROJECT_CONFIG_DIR
 cat bashrc >> ${HOME_DIR}/.bashrc
+cp tmux.conf ${HOME_DIR}/.tmux.conf
+cp screenrc ${HOME_DIR}/.screenrc
+
 mkdir -p $VIM_COLOR_DIR
 cp gruvbox.vim $VIM_COLOR_DIR
 echo -e "## [STAGE] ...Done\n"
+
+
 
 echo -e "## [STAGE] Installing Vundle for manegement of Vim plugins..."
 if [ ! -d ${HOME_DIR}/.vim/bundle/Vundle.vim ] ; then
@@ -110,11 +136,15 @@ if [ ! -d ${HOME_DIR}/.vim/bundle/Vundle.vim ] ; then
 fi
 echo -e "## [STAGE] ...Done\n"
 
+
+
 echo -e "## [STAGE] Installing Vim plugins defined in vimrc..."
-cd $PROJECT_DIR
+cd $PROJECT_CONFIG_DIR
 cp vimrc ${HOME_DIR}/.vimrc
 vim +PluginInstall +qa
 echo -e "## [STAGE] ...Done\n"
+
+
 
 echo -e "## [STAGE] Installing cmake, then compile YouCompleteMe..."
 brew install cmake
@@ -124,21 +154,27 @@ cd $HOME_DIR/.vim/bundle/YouCompleteMe
              --gocode-completer
 echo -e "## [STAGE] ...Done\n"
 
+
+
 echo -e "## [STAGE] Importing color scheme for iTerm2 and terminal (please click 'OK' on the popup window..."
-cd $PROJECT_DIR
+cd $PROJECT_COLOR_DIR
 open gruvbox-dark.itermcolors
 open gruvbox-dark.terminal
 open gruvbox-light.itermcolors
 open gruvbox-light.terminal
 echo -e "## [STAGE] ...Done\n"
 
+
+
 echo -e "## [STAGE] Installing oh-my-zsh, and copying configuration file..."
 if [ ! -d ${HOME_DIR}/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
-cd $PROJECT_DIR
+cd $PROJECT_CONFIG_DIR
 cp zshrc ${HOME_DIR}/.zshrc
 echo -e "## [STAGE] ...Done\n"
+
+
 
 echo -e "## [DONE] Finish installation, please enjoy!!"
 echo -e "
@@ -147,4 +183,5 @@ echo -e "
     | __| | | | '_ \\ / _ \\____\\ \\ / / | '_ \` _ \\
     | |_| |_| | |_) |  __/_____\\ V /| | | | | | |
      \\__|\\__,_|_.__/ \\___|      \\_/ |_|_| |_| |_|\n"
+
 
