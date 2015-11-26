d8e04ee0516515d802599b099798d51433f82217
put-your-name-here <put-your-email-here>
2015-11-25 21:58:54 -0800

    Improved linux installation process

--- install-linux.sh
+++ install-linux.sh
@@ -28,14 +28,17 @@ println "...Done"
 
 
 println "Cloning repo to home directories..."
-rm -rf $PROJECT_DIR
-git clone https://github.com/tolinwei/tube-vim.git $PROJECT_DIR
+rm -rf ${PROJECT_DIR}
+git clone https://github.com/tolinwei/tube-vim.git ${PROJECT_DIR}
 println "...Done\n"
 
 
 println "Backing up existing Vim, screen, tmux settings and bashrc..."
-cd $HOME_DIR
+cd ${HOME_DIR}
 date_time=`date +"%y-%m-%d-%H:%M"`
+if [ -e .bashrc ]; then
+    println "Skipping .bashrc as it's existed"
+fi
 if [ -e .vimrc ]; then
     println "Backing up .vimrc to .vimrc.bak-${date_time}"
     mv .vimrc .vimrc.bak-${date_time}
@@ -52,40 +55,33 @@ if [ -e .tmux.conf ]; then
     println "Backing up .tmux.conf to .tmux.conf.bak-${date_time}"
     mv .tmux.conf .tmux.conf.bak-${date_time}
 fi
-if [ -e .bashrc ]; then
-    println "Skipping .bashrc as it's existed"
-fi
 println "...Done"
 
 
 println "Copying configuration file for bash, tmux, screen and color scheme for Vim..."
-cd $PROJECT_CONFIG_DIR
+cd ${PROJECT_CONFIG_DIR}
 if [ ! -f ${HOME_DIR}/.bashrc ]; then
     cp bashrc ${HOME_DIR}/.bashrc
 fi
 cp tmux.conf ${HOME_DIR}/.tmux.conf
 cp screenrc ${HOME_DIR}/.screenrc
 
-mkdir -p $VIM_COLOR_DIR
-cd $PROJECT_COLOR_DIR
-cp gruvbox.vim $VIM_COLOR_DIR
+mkdir -p ${VIM_COLOR_DIR}
+cd ${PROJECT_COLOR_DIR}
+cp gruvbox.vim ${VIM_COLOR_DIR}
 println "...Done"
 
 
 println "Installing junegunn/vim-plug as Vim plugins manager..."
-# if [ ! -d ${HOME_DIR}/.vim/bundle/Vundle.vim ]; then
-#     git clone https://github.com/gmarik/Vundle.vim.git ${HOME_DIR}/.vim/bundle/Vundle.vim
-# fi
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 println "...Done"
 
 
 println "Installing Vim plugins defined in vimrc..."
-cd $PROJECT_CONFIG_DIR
+cd ${PROJECT_CONFIG_DIR}
 cp vimrc-linux ${HOME_DIR}/.vimrc
 vim +PlugInstall +qa
-# vim +PluginInstall +qall
 println "...Done\n"
 
 
