96b0cb79a907134bd1cc1179a6d631b0e4c686e6
put-your-name-here <put-your-email-here>
2015-11-25 21:57:26 -0800

    Improved oh-my-zsh installation process

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ println "[Start] Seting up integrated shell environment..."
 println "Defining directory variables..."
 HOME_DIR=~
 PROJECT_DIR=${HOME_DIR}/.tube-vim
+OH_MY_ZSH_DIR=${HOME_DIR/.oh-my-zsh
 PROJECT_COLOR_DIR=${PROJECT_DIR}/colors
 PROJECT_CONF_DIR=${PROJECT_DIR}/config
 VIM_COLOR_DIR=${HOME_DIR}/.vim/colors
@@ -43,8 +44,8 @@ println "...Done"
 
 
 println "Cloning repo to home directories..."
-rm -rf $PROJECT_DIR
-git clone https://github.com/tolinwei/tube-vim.git $PROJECT_DIR
+rm -rf ${PROJECT_DIR}
+git clone https://github.com/tolinwei/tube-vim.git ${PROJECT_DIR}
 println "...Done"
 
 
@@ -56,6 +57,9 @@ println "...Done"
 println "Backing up existing Vim, screen, tmux settings, bashrc and zsh related..."
 cd ${HOME_DIR}
 date_time=`date +"%y-%m-%d-%H:%M"`
+if [ -e .bashrc ]; then  # Special process for .bashrc
+    println "Skipping .bashrc as it's existed"
+fi
 if [ -e .vimrc ]; then
     println "Backing up .vimrc to .vimrc.bak-${date_time}"
     mv .vimrc .vimrc.bak-${date_time}
@@ -72,40 +76,10 @@ if [ -e .tmux.conf ]; then
     println "Backing up .tmux.conf to .tmux.conf.bak-${date_time}"
     mv .tmux.conf .tmux.conf.bak-${date_time}
 fi
-if [ -e .bashrc ]; then  # Special process for .bashrc
-    println "Skipping .bashrc as it's existed"
-fi
-if [ -e .zshenv ]; then
-    println "Backing up .zshenv to .zshenv.bak-${date_time}"
-    mv .zshenv .zshenv.bak-${date_time}
-fi
-if [ -e .zprofile ]; then
-    println "Backing up .zprofile to .zprofile.bak-${date_time}"
-    mv .zprofile .zprofile.bak-${date_time}
-fi
-if [ -e .zshrc ]; then
-    println "Backing up .zshrc to .zshrc.bak-${date_time}"
-    mv .zshrc .zshrc.bak-${date_time}
-fi
-if [ -e .zpreztorc ]; then
-    println "Backing up .zpreztorc to .zpreztorc.bak-${date_time}"
-    mv .zpreztorc .zpreztorc.bak-${date_time}
-fi
-if [ -e .zlogin ]; then
-    println "Backing up .zlogin to .zlogin.bak-${date_time}"
-    mv .zlogin .zlogin.bak-${date_time}
-fi
-if [ -e .zlogout ]; then
-    println "Backing up .zlogout to .zlogout.bak-${date_time}"
-    mv .zlogout .zlogout.bak-${date_time}
-fi
 println "...Done"
 
 
 println "Installing Vim and tmux via Homebrew..."
-# Needs to restart shell session to make installation works
-# brew install macvim --with-lua
-# brew linkapps macvim
 brew install vim
 brew install tmux
 println "...Done"
@@ -137,9 +111,6 @@ println "...Done"
 
 
 println "Installing junegunn/vim-plug as Vim plugins manager..."
-# if [ ! -d ${HOME_DIR}/.vim/bundle/Vundle.vim ]; then
-#     git clone https://github.com/gmarik/Vundle.vim.git ${HOME_DIR}/.vim/bundle/Vundle.vim
-# fi
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 println "...Done"
@@ -148,7 +119,6 @@ println "...Done"
 println "Installing Vim plugins defined in vimrc..."
 cp ${PROJECT_CONF_DIR}/vimrc ${HOME_DIR}/.vimrc
 vim +PlugInstall +qa
-# vim +PluginInstall +qa
 println "...Done"
 
 
@@ -160,9 +130,10 @@ println "...Done"
 
 
 println "Installing oh-my-zsh, and copying configuration file..."
-if [ ! -d ${HOME_DIR}/.oh-my-zsh ]; then
-    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ -d ${OH_MY_ZSH_DIR} ]; then
+    rm -rf ${OH_MY_ZSH_DIR}
 fi
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp ${PROJECT_CONF_DIR}/zshrc ${HOME_DIR}/.zshrc
 println "...Done"
 
