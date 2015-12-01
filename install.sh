06366cd74c3cb09279c3de8ac174ac36ab7f1df8
put-your-name-here <put-your-email-here>
2015-12-01 03:34:01 -0800

    .

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 function println {
     echo "[" `date` "]" $1
-    if [ "$1" == "...Done" ]; then
-        echo
-    fi
+    # if [ "$1" == "...Done" ]; then
+        # echo
+    # fi
 }
 
 # Define colors for prompt if terminal supports
@@ -26,7 +26,7 @@ echo -e "${YELLOW}
 | |_| |_| | |_) |  __/_____\\__ \\ | | |  __/ | |
  \\__|\\__,_|_.__/ \\___|     |___/_| |_|\\___|_|_|${NORMAL}\n"
 
-println "${YELLOW}[Start] Seting up integrated shell environment...${NORMAL}"
+println "${YELLOW}[Start] Seting up integrated shell environment...${NORMAL}\n"
 
 
 println "${YELLOW}Defining directory variables...${NORMAL}"
@@ -36,7 +36,7 @@ OH_MY_ZSH_DIR=${HOME_DIR}/.oh-my-zsh
 PROJECT_COLOR_DIR=${PROJECT_DIR}/colors
 PROJECT_CONF_DIR=${PROJECT_DIR}/config
 VIM_COLOR_DIR=${HOME_DIR}/.vim/colors
-println "${YELLOW}...Done${NORMAL}"
+println "${YELLOW}...Done${NORMAL}\n"
 
 
 println "${YELLOW}Installing brew, git for OS X...${NORMAL}"
@@ -51,24 +51,24 @@ else
 fi
 println "Installing git via Homebrew...${NORMAL}"
 which -s git || brew install git
-println "${YELLOW}...Done${NORMAL}"
+println "${YELLOW}...Done${NORMAL}\n"
 
 
 println "${YELLOW}Cloning repo to home directories...${NORMAL}"
 rm -rf ${PROJECT_DIR}
 git clone https://github.com/tolinwei/tube-shell.git ${PROJECT_DIR}
-println "${YELLOW}...Done${NORMAL}"
+println "${YELLOW}...Done${NORMAL}\n"
 
 
 println "${YELLOW}Installing Command Line Tools for OS X...${NORMAL}"
 xcode-select --install
-println "${YELLOW}...Done${NORMAL}"
+println "${YELLOW}...Done${NORMAL}\n"
 
 
 println "${YELLOW}Installing Vim & tmux via Homebrew...${NORMAL}"
 brew install vim
 brew install tmux
-println "${YELLOW}...Done${NORMAL}"
+println "${YELLOW}...Done${NORMAL}\n"
 
 
 println "${YELLOW}Backing up & copying configuration files for Bash, Vim, screen and tmux...${NORMAL}"
@@ -102,7 +102,7 @@ cp ${PROJECT_CONF_DIR}/screenrc ${HOME_DIR}/.screenrc
 println "Copying Vim color scheme to ${VIM_COLOR_DIR}"
 mkdir -p ${VIM_COLOR_DIR}
 cp ${PROJECT_COLOR_DIR}/gruvbox.vim ${VIM_COLOR_DIR}
-println "${YELLOW}...Done${NORMAL}"
+println "${YELLOW}...Done${NORMAL}\n"
 
 
 println "${YELLOW}Installing Exuberant Ctags to support tagbar via Homebrew...${NORMAL}"
@@ -113,18 +113,18 @@ if [ ${CTAGS_DIR} != '/usr/local/bin/ctags' ]; then
 else
     println "Exuberant Ctags (Homebrew version) has already installed"
 fi
-println "${YELLOW}...Done${NORMAL}"
+println "${YELLOW}...Done${NORMAL}\n"
 
 
 println "${YELLOW}Installing junegunn/vim-plug as Vim plugins manager...${NORMAL}"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-println "${YELLOW}...Done"
+println "${YELLOW}...Done${NORMAL}\n"
 
 
 println "${YELLOW}Installing Vim plugins defined in .vimrc..."
 vim +PlugInstall +qa
-println "${YELLOW}...Done${NORMAL}"
+println "${YELLOW}...Done${NORMAL}\n"
 
 
 println "${YELLOW}Installing oh-my-zsh & copying configuration file...${NORMAL}"
@@ -136,14 +136,14 @@ sh -c "$(curl -fsSL https://raw.github.com/tolinwei/oh-my-zsh/master/tools/insta
     # exit"
 println "Copying zshrc to home directory"
 cp ${PROJECT_CONF_DIR}/zshrc ${HOME_DIR}/.zshrc
-println "${YELLOW}...Done${NORMAL}"
+println "${YELLOW}...Done${NORMAL}\n"
 
 
 println "${YELLOW}Importing color scheme for iTerm2 & Terminal.app...${NORMAL}"
 cd ${PROJECT_COLOR_DIR}
 open gruvbox-dark.itermcolors
 open gruvbox-dark.terminal
-println "${YELLOW}...Done${NORMAL}"
+println "${YELLOW}...Done${NORMAL}\n"
 
 
 println "${YELLOW}[End] Finish! Please restart your terminal emulator to enjoy!!${NORMAL}"
